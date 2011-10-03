72afa9c210e211e897a0287c7a10544d30beba6e
Konzertheld <konzertheld@konzertheld.de>
2011-10-03 18:22:50 +0100

    Start developing tools for file loading.

---
+++ SharpShuffle/Filemanagement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SharpShuffle
+{
+    class Filemanagement
+    {
+        /// <summary>
+        /// Read meta from files and return a dictionary where you can lookup the meta by the file.
+        /// </summary>
+        /// <param name="path">Folder to look for songs.</param>
+        /// <returns></returns>
+        public static Dictionary<string, Song> Read(string path)
+        {
+            Dictionary<string, Song> data = new Dictionary<string, Song>();
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                if (Program.ALLOWED_EXTENSIONS.Contains(Path.GetExtension(file).Substring(1)))
+                    data.Add(file, Song.ReadTags(file));
+            }
+            return data;
+        }
+
+        public static void ProcessFolder(string path)
+        {
+        }
+    }
+}
