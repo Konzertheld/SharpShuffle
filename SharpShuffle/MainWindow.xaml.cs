4a1b5f08453d72f5aefd2a0c2bd0509c0d8f03e5
Konzertheld <konzertheld@konzertheld.de>
2011-10-04 02:40:04 +0100

    UI: Start designing. Test some stuff.

--- SharpShuffle/MainWindow.xaml.cs
+++ SharpShuffle/MainWindow.xaml.cs
@@ -19,19 +19,48 @@ namespace SharpShuffle
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Collections.ObjectModel.ObservableCollection<Song> songs = new System.Collections.ObjectModel.ObservableCollection<Song>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            lsvFilterPool.ItemsSource = Startup.ActiveDB.PoolList();
+
+            songs = new System.Collections.ObjectModel.ObservableCollection<Song>(Startup.ActiveDB.LoadSongs("Endsortierung"));
+            lsvView.ItemsSource = songs;
+            foreach (SONGMETA meta in Enum.GetValues(typeof(SONGMETA)))
+            {
+                ((GridView)(lsvView.View)).Columns.Add(new GridViewColumn());
+                ((GridView)(lsvView.View)).Columns.Last().Header = meta.ToString();
+                ((GridView)(lsvView.View)).Columns.Last().DisplayMemberBinding = new Binding(meta.ToString());
+            }
         }
 
-        private void Grid_MouseUp(object sender, MouseButtonEventArgs e)
+        private void lsvView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            List<Song> songs = new List<Song>();
-            songs.Add(new Song());
-            songs.Add(new Song());
-            songs.Add(new Song());
             
-            lsvView.Items.Add(songs[0]);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            songs.RemoveAt(0);
+        }
+
+        private void lsvView_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void lsvView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show(Startup.ActiveDB.GetFileForSong(lsvView.Items[lsvView.SelectedIndex] as Song));
+        }
+
+        private void lsvView_KeyUp(object sender, KeyEventArgs e)
+        {
+            
         }
     }
 }
