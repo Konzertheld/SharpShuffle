b410ba786d28fa1bb7502335f8ecf98c39da7d70
Konzertheld <konzertheld@konzertheld.de>
2011-11-08 21:24:10 +0100

    New config class

---
+++ SharpShuffle/Config.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpShuffle
+{
+    public enum PlayActions
+    {
+        AddAndPlayNow,
+        AddAndPlayNext,
+        Add,
+        PlayNowUseView,
+        PlayNowReplacePlaylist
+    }
+
+    public static class Config
+    {
+        private static Dictionary<ModifierKeys, PlayActions> PlayKeys;
+
+        static Config()
+        {
+            PlayKeys = new Dictionary<ModifierKeys, PlayActions>();
+            PlayKeys[ModifierKeys.None] = PlayActions.AddAndPlayNow;
+            PlayKeys[ModifierKeys.Shift] = PlayActions.AddAndPlayNext;
+            PlayKeys[ModifierKeys.Alt] = PlayActions.Add;
+            PlayKeys[ModifierKeys.Control] = PlayActions.PlayNowUseView;
+        }
+
+        public static PlayActions GetPlayAction(ModifierKeys modifier)
+        {
+            return PlayKeys[modifier];
+        }
+
+        public static void SetPlayAction(ModifierKeys modifier, PlayActions action)
+        {
+            if (modifier != ModifierKeys.Windows)
+                PlayKeys[modifier] = action;
+        }
+    }
+}
